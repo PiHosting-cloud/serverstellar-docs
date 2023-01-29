cea649fee12810386ba7ba7de573570f3da33c5a
BestRazer <65088083+BestRazer@users.noreply.github.com>
2023-01-29 19:31:31 +0100

    fixed weird title

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Documentation ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Documentation for our free hosting">
       <HomepageHeader />
       <main>
